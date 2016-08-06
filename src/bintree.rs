79903f2a2ea2b5d7c6c03e2602f438244b2bbe53
Andrea Jemmett <andreajemmett@gmail.com>
2016-08-06 18:12:24 +0200

    store value in nodes

--- src/bintree.rs
+++ src/bintree.rs
@@ -2,28 +2,37 @@ use hashable::*;
 
 
 #[derive(Debug)]
-pub enum BinMerkleTree<'a> {
+pub enum BinMerkleTree<'a, T: 'a> {
     Empty,
-    Node(String, &'a BinMerkleTree<'a>, &'a BinMerkleTree<'a>)
+    Node(String, T, &'a BinMerkleTree<'a, T>, &'a BinMerkleTree<'a, T>)
 }
 
-impl<'a> BinMerkleTree<'a> {
-    pub fn new<T: Hashable>(value: T, left: &'a BinMerkleTree<'a>, right: &'a BinMerkleTree<'a>) -> BinMerkleTree<'a> {
+impl<'a, T> BinMerkleTree<'a, T>
+    where T: Hashable + Copy {
+
+    pub fn new(value: T, left: &'a BinMerkleTree<'a, T>, right: &'a BinMerkleTree<'a, T>) -> BinMerkleTree<'a, T> {
         let mut children_hash = String::new();
         match left {
-            &BinMerkleTree::Node(ref hv, _, _) => children_hash = children_hash + &hv,
+            &BinMerkleTree::Node(ref hv, v, _, _) => {
+                assert_eq!(hash(v), *hv);
+                children_hash = children_hash + &hv;
+            }
             _ => {}
         }
         match right {
-            &BinMerkleTree::Node(ref hv, _, _) => children_hash = children_hash + &hv,
+            &BinMerkleTree::Node(ref hv, v, _, _) => {
+                assert_eq!(hash(v), *hv);
+                children_hash = children_hash + &hv;
+            }
             _ => {}
         }
 
         let hash_value = if children_hash.len() > 0 { hash(hash(value) + &children_hash) } else { hash(value) };
         BinMerkleTree::Node(
-            hash_value, left, right
+            hash_value, value, left, right
         )
     }
+
 }
 
 
@@ -38,7 +47,7 @@ mod tests {
         let empty = BinMerkleTree::Empty;
         let root = BinMerkleTree::new("", &empty, &empty);
         match root {
-            BinMerkleTree::Node(hv, &BinMerkleTree::Empty, &BinMerkleTree::Empty) =>
+            BinMerkleTree::Node(hv, _, &BinMerkleTree::Empty, &BinMerkleTree::Empty) =>
                 assert_eq!(hv, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
             _ => assert!(false),
         }
@@ -50,7 +59,7 @@ mod tests {
         let child = BinMerkleTree::new("", &empty, &empty);
         let root = BinMerkleTree::new("", &child, &empty);
         match root {
-            BinMerkleTree::Node(ref hv, &BinMerkleTree::Node(ref chv, _, _), &BinMerkleTree::Empty) => {
+            BinMerkleTree::Node(ref hv, _, &BinMerkleTree::Node(ref chv, _, _, _), &BinMerkleTree::Empty) => {
                 assert_eq!(hv, "3b7546ed79e3e5a7907381b093c5a182cbf364c5dd0443dfa956c8cca271cc33");
                 assert_eq!(chv, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
             }
@@ -64,7 +73,7 @@ mod tests {
         let child = BinMerkleTree::new("", &empty, &empty);
         let root = BinMerkleTree::new("", &child, &child);
         match root {
-            BinMerkleTree::Node(hv, _, _) =>
+            BinMerkleTree::Node(hv, _, _, _) =>
                 assert_eq!(hv, "74313561d1897af3dc03f4fae174960d28968f92b49230523faca462b848db60"),
             _ => assert!(false)
         }
--- src/list.rs
+++ src/list.rs
@@ -2,18 +2,25 @@ use hashable::*;
 
 
 #[derive(Debug)]
-pub enum MerkleList<'a> {
+pub enum MerkleList<'a, T: 'a> {
     Empty,
-    Node(String, &'a MerkleList<'a>)
+    Node(String, T, &'a MerkleList<'a, T>)
 }
 
-impl<'a> MerkleList<'a> {
-    pub fn new<T: Hashable>(value: T, child: &'a MerkleList<'a>) -> MerkleList<'a> {
-        match *child {
-            MerkleList::Empty => MerkleList::Node(hash(value), child),
-            MerkleList::Node(ref hv, _) => MerkleList::Node(hash(hash(value) + &hv), child)
+impl<'a, T> MerkleList<'a, T>
+    where T: Hashable + Copy {
+
+    pub fn new(value: T, child: &'a MerkleList<'a, T>) -> MerkleList<'a, T> {
+        let new_hash = hash(value);
+        match child {
+            &MerkleList::Empty => MerkleList::Node(new_hash, value, child),
+            &MerkleList::Node(ref hv, ref cv, _) => {
+                assert_eq!(hash(*cv), *hv);
+                MerkleList::Node(hash(new_hash + &hv), value, child)
+            }
         }
     }
+
 }
 
 
@@ -28,8 +35,10 @@ mod tests {
         let empty = MerkleList::Empty;
         let list = MerkleList::new("", &empty);
         match list {
-            MerkleList::Node(hv, &MerkleList::Empty) =>
-                assert_eq!(hv, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
+            MerkleList::Node(hv, v, &MerkleList::Empty) => {
+                assert_eq!(hv, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+                assert_eq!(v, "")
+            }
             _ => assert!(false)
         }
     }
@@ -40,8 +49,10 @@ mod tests {
         let child = MerkleList::new("", &empty);
         let list = MerkleList::new("", &child);
         match list {
-            MerkleList::Node(ref hv, &MerkleList::Node(ref chv, &MerkleList::Empty)) => {
+            MerkleList::Node(ref hv, v, &MerkleList::Node(ref chv, cv, &MerkleList::Empty)) => {
+                assert_eq!(v, "");
                 assert_eq!(hv, "3b7546ed79e3e5a7907381b093c5a182cbf364c5dd0443dfa956c8cca271cc33");
+                assert_eq!(cv, "");
                 assert_eq!(chv, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
             }
             _ => assert!(false)
